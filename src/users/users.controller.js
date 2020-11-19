7fc21f4f9cb79bae845ae246db645a53ae9782a8
Nataliia <n3v3st4@gmail.com>
2020-11-20 01:51:03 +0200

    subscription update done

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,6 +1,22 @@
+const { Conflict } = require("../helpers/errors");
+const { UserModel } = require("./users.model");
 
 exports.getCurrentUser = async (req, res, next) => {
     const { id, email, subscription } = req.user;
     
     res.status(200).send({ id, email, subscription });
+};
+
+exports.updateUserSubscription = async (req, res, next) => {
+    try {
+        const { id, subscription } = req.user;
+        const subscriptionTypes = UserModel.schema.tree.subscription.enum;
+        if (!subscriptionTypes.includes(req.body.subscription)) { throw new Conflict('There is no such subscription type'); };
+
+        if (req.body.subscription === subscription) { throw new Conflict('You must choose different type of subscription');};
+        const updatedUser = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+        return res.status(200).send(updatedUser);
+    } catch (error) {
+        next(error);
+    }
 }
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const { authorize } = require('../helpers/authorize.middleware');
-const { getCurrentUser } = require('./users.controller');
+const { getCurrentUser, updateUserSubscription } = require('./users.controller');
 
 const router = Router();
 
 router.get('/current', authorize, getCurrentUser);
+router.patch('/current', authorize, updateUserSubscription);
 
 exports.usersRouter = router;
